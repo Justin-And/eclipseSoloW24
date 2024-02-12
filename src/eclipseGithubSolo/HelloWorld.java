42ac777dd716a999bfbffbb7c3472a2be30b1042
Isaac <imulliso@emich.edu>
2024-02-12 10:51:46 -0500

    Alternate second line

--- src/eclipseGithubSolo/HelloWorld.java
+++ src/eclipseGithubSolo/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Github Project");
-		System.out.println("This is a second statement");
+		System.out.println("This is an alternate second statement");
 	}
 }
